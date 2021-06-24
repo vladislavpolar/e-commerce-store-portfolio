5af75b8c73a122293e4f73c136e29d59143cfcac
vladislavpolar <vlad.tsybuliak.vt@gmail.com>
2021-06-24 11:33:23 +0300

    creating TodoForm with input using hooks

---
+++ src/components/TodoForm.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function TodoForm() {
+  const [input, setInput] = useState('')
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default TodoForm
